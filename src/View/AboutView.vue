745d889f667be32c90b252f6c0316925402043eb
FloAlbaux <florence.albaux@gmail.com>
2023-05-27 11:42:39 +0200

    Simple nav routing

---
+++ src/View/AboutView.vue
@@ -0,0 +1,13 @@
+<template>
+    <main>
+        <p>
+            Je me suis lancée dans l'apprentissage du routing avec Vue.js, et je dois dire que c'est passionnant ! Pour rendre mes exercices encore plus intéressants, j'ai décidé d'utiliser l'API Pokémon. Après tout, qui peut résister à l'attrait des Pokémon ?
+            Avec le routing, je peux créer différentes pages pour afficher des informations sur les Pokémon. J'utilise Vue.js pour dynamiquement récupérer les données des Pokémon à partir de l'API et les afficher de manière structurée. Chaque Pokémon est représenté par une boîte stylisée avec une petite ombre et des bords arrondis, ce qui les rend vraiment adorables.
+            En utilisant le routing, je peux naviguer entre différentes vues pour afficher des détails sur chaque Pokémon, comme leur nom, leur type et leurs capacités. Je peux également ajouter des fonctionnalités supplémentaires, comme la recherche de Pokémon spécifiques ou la possibilité de filtrer par type.
+            L'API Pokémon est une mine d'or en termes de données. Je peux accéder à des informations détaillées sur chaque Pokémon, y compris leurs statistiques, leurs évolutions et même leurs images. Cela me permet de créer des pages riches en contenu et de donner vie à mon application.
+            J'avoue que travailler avec les Pokémon rend cet apprentissage encore plus excitant. Les Pokémon ont toujours été une passion pour moi, depuis mon enfance. Ils ont des designs uniques, des pouvoirs incroyables et une variété infinie. Pouvoir les explorer et les afficher dans mon application est une expérience vraiment amusante.
+            Grâce à Vue.js et à l'API Pokémon, j'ai trouvé une nouvelle façon de combiner ma passion pour les Pokémon avec mes compétences en développement web. C'est une expérience stimulante et divertissante qui me permet d'apprendre de nouvelles techniques tout en m'amusant avec les créatures adorables et puissantes que sont les Pokémon. Après tout, les Pokémon sont bien plus qu'une simple source d'inspiration - ils sont cool !"
+            J'espère que cela correspond à ce que vous recherchiez. Si vous avez besoin de plus d'informations ou si vous avez d'autres demandes, n'hésitez pas à me le faire savoir !
+        </p>
+    </main>
+</template>
